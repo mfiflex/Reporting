1f5601dbe0e6dd43dc0a53088c087d82022bf69e
unknown <sfulzele@SFULZELE-LAP.st-users.us.oracle.com>
2012-08-21 22:39:14 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MFiFlexInternetBanking::Application.config.secret_token = '7d1d1d69ee0c554871944fe6efbf63535462b54d13446b06b8ec46d7807e6dc2acdcbaf0074f9799d25905dc8c38e90b46713c5615bd3d3bc714ae783a046389'
