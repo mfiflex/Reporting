9bb94d98879b0ef74f597b1d0abeba0cb80a9d98
Snehal Fulzele <snehal.fulzele@mfiflex.com>
2012-09-10 22:30:43 -0700

    database config done..

--- app/scheduler/import_data.rb
+++ app/scheduler/import_data.rb
@@ -12,11 +12,14 @@ class ImportSalesforceToPG
     #Read config in the caller code
     config = YAML.load_file(File.join(::Rails.root, 'config', 'databasedotcom.yml'))    
     #get Connection
-    conn = PG.connect('localhost', '5432', '','','mfiforce', 'postgres','' )
+    #conn = PG.connect('localhost', '5432', '','','mfiforce', 'postgres','' )
+    databaseConfig =  Rails.configuration.database_configuration
+    conn = PG.connect(databaseConfig[Rails.env]["host"], databaseConfig[Rails.env]["port"], '','',databaseConfig[Rails.env]["database"], databaseConfig[Rails.env]["username"],databaseConfig[Rails.env]["password"] )
     
     importC = ImportClient.new
-    #importC.import(config[:username],config[:password],conn)
-    puts config[:username]
+    # importC.import(config[:username],config[:password],conn)
+    # Loading config this way is not applicable for salesforcE_bulk gem.
+    # Might need to add logic to read data from multiple salesforce instances 
     #TODO read salesforce user/password info from a config file
     importC.import('admin@30df.org','Merc1243HGRcayiE38dzluu4LkACcfOjy',conn)
       
