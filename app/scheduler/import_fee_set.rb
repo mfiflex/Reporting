814e7dc30789e5cc3d16cfe5d9516bc4a5bc67ce
Gaurav Singh <gaurav.singh2012@mfiflex.co.in>
2013-01-21 23:02:09 +0530

    Delta Fix Date

--- app/scheduler/import_fee_set.rb
+++ app/scheduler/import_fee_set.rb
@@ -13,10 +13,12 @@ class ImportFeeSet
     salesforce = SalesforceBulk::Api.new(salesforceUserName,salesforcePassword)
     
     # Query using BULK API
+    test = getFeeSetQuery+whereClause
+    puts test
     res = salesforce.query(getFeeSetObjName,getFeeSetQuery+whereClause)
     
     q_result = res.result.records
-    
+    puts q_result[0]
     #Upsert into Postgres
     upsertRecords(getFeeSetObjName,q_result,pgConn,salesforceOrgId)
     
